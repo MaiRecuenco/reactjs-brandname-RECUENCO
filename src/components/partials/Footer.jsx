4ca87a0a0c68d1e083f017a0b3c9e2b7e25efe33
aaairam <magildaceria@gmail.com>
2025-07-11 13:24:49 +0800

    commit

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       {/* Footer */}
-      <footer className="bg-gray-800 text-white py-12 px-4">
+      <footer id="footer" className="bg-gray-800 text-white py-12 px-4">
         <div className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
